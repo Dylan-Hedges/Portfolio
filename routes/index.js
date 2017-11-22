5a604e8d3f44463ddb81e9dda6a04fb55309c50b
dylan <d.hedges1@hotmail.co.uk>
2017-11-22 13:06:07 +0000

    Updated styling for detailed blogpost page and notification messages

--- routes/index.js
+++ routes/index.js
@@ -63,7 +63,7 @@ router.post("/login",  passport.authenticate("local",
 //LOGOUT ROUTE
 router.get("/logout", function(req, res){
    req.logout();
-   req.flash("success", "Logged you out");
+   req.flash("success", "Logout successful.");
    res.redirect("/blogposts");
 });
 
