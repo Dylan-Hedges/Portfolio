20b80352cac446641cb95260c6b83ecb0b8e7518
dylan <d.hedges1@hotmail.co.uk>
2018-01-21 22:53:18 +0000

    Added environment variables

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  recipient: process.env.RECIPIENT,
+  refreshToken: process.env.REFRESHTOKEN,
+  accessToken: process.env.ACCESSTOKEN,
+  mlabconnection: process.env.MLAB_CONNECTION,
+  admin: process.env.ADMIN
+}
+
+module.exports = config;
