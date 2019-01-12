2f3b7d8cd19f46ba2d45b8633fee8897d6515527
dylan <d.hedges1@hotmail.co.uk>
2019-01-12 14:06:55 +0000

    Added CSS animations to about page, added underline for titles

--- public/stylesheets/script.js
+++ public/stylesheets/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+    //------Landing Page-------
+    //Header
+    $('.animate-wp-4').waypoint(function(direction) {
+        $('.animate-wp-4').addClass('animated fadeInUp');
+    }, {
+        offset: '100%'
+    });
+    //Interest Icons
     $('.animate-wp-1').waypoint(function(direction) {
         $('.animate-wp-1').addClass('animated fadeInUp');
     }, {
@@ -9,28 +17,92 @@ $(document).ready(function () {
     }, {
         offset: '85%'
     });
+    //Skill Icons
     $('.animate-wp-3').waypoint(function(direction) {
         $('.animate-wp-3').addClass('animated fadeInDown');
     }, {
         offset: '80%'
     });
-    $('.animate-wp-4').waypoint(function(direction) {
-        $('.animate-wp-4').addClass('animated fadeInDown');
-    }, {
-        offset: '77%'
-    });
+    //--------Contact Page-----
     $('.animate-wp-5').waypoint(function(direction) {
         $('.animate-wp-5').addClass('animated bounce');
     }, {
         offset: '100%'
     });
+    //-----------About--------
+    //Image
     $('.animate-wp-6').waypoint(function(direction) {
-        $('.animate-wp-6').addClass('animated fadeInUp');
+        $('.animate-wp-6').addClass('animated fadeInLeft');
     }, {
         offset: '100%'
     });
+    //Text
     $('.animate-wp-7').waypoint(function(direction) {
-        $('.animate-wp-7').addClass('animated fadeInUp');
+        $('.animate-wp-7').addClass('animated fadeInRight');
+    }, {
+        offset: '100%'
+    });
+    //---BSC---
+    //Text
+    $('.animate-wp-8').waypoint(function(direction) {
+        $('.animate-wp-8').addClass('animated fadeInLeft');
+    }, {
+       offset: '50%'
+    });
+    //Image
+    $('.animate-wp-9').waypoint(function(direction) {
+        $('.animate-wp-9').addClass('animated fadeInRight');
+    }, {
+      offset: '50%'
+    });
+    //---AWS---
+    //Image
+    $('.animate-wp-10').waypoint(function(direction) {
+        $('.animate-wp-10').addClass('animated fadeInLeft');
+    }, {
+      offset: '65%'
+    });
+    //Text
+    $('.animate-wp-11').waypoint(function(direction) {
+        $('.animate-wp-11').addClass('animated fadeInRight');
+    }, {
+      offset: '65%'
+    });
+    //---PwC---
+    //Text
+    $('.animate-wp-12').waypoint(function(direction) {
+        $('.animate-wp-12').addClass('animated fadeInLeft');
+    }, {
+        offset: '65%'
+    });
+    //Image
+    $('.animate-wp-13').waypoint(function(direction) {
+        $('.animate-wp-13').addClass('animated fadeInRight');
+    }, {
+        offset: '65%'
+    });
+    //---Intel---
+    //Image
+    $('.animate-wp-14').waypoint(function(direction) {
+        $('.animate-wp-14').addClass('animated fadeInLeft');
+    }, {
+        offset: '65%'
+    });
+    //Text
+    $('.animate-wp-15').waypoint(function(direction) {
+        $('.animate-wp-15').addClass('animated fadeInRight');
+    }, {
+        offset: '65%'
+    });
+    
+    
+    $('.animate-wp-16').waypoint(function(direction) {
+        $('.animate-wp-16').addClass('animated zoomIn');
+    }, {
+        offset: '100%'
+    });
+    $('.animate-wp-17').waypoint(function(direction) {
+        $('.animate-wp-17').addClass('animated fadeInDown');
     }, {
         offset: '100%'
     });
